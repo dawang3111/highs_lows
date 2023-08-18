231183e880f69ea10e1ee519f0e29f50df2b1b24
1797094806@qq.com <1797094806@qq.com>
2023-08-18 16:51:26 +0800

    8.18

--- 16/highs_lows.py
+++ 16/highs_lows.py
@@ -3,15 +3,16 @@ from datetime import datetime
 from matplotlib import pyplot as plt
 
 # the chart about sitka
-#filename = 'sitka_weather_2021_full.csv'
+filename_sitka = 'sitka_weather_2021_full.csv'
 # the chart about death_valley
-filename = 'death_valley_2021_full.csv'
-with open(filename) as f:
+filename_death_valley = 'death_valley_2021_full.csv'
+# put data from death_valley into chart
+with open(filename_death_valley) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    highs = []
-    dates = []
-    lows = []
+    highs_death = []
+    dates_death = []
+    lows_death = []
     for row in reader:
         try:
             date = datetime.strptime(row[2], "%Y-%m-%d")
@@ -20,14 +21,35 @@ with open(filename) as f:
         except ValueError:
             print(date, 'Missing data')
         else:
-            highs.append(high)
-            dates.append(date)
-            lows.append(low)
+            highs_death.append(high)
+            dates_death.append(date)
+            lows_death.append(low)
+# put data from stika into chart
+with open(filename_sitka) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    highs_sitka = []
+    dates_sitka = []
+    lows_sitka = []
+    for row in reader:
+        try:
+            date = datetime.strptime(row[2], "%Y/%m/%d")
+            high = int(row[7])
+            low = int(row[8])
+        except ValueError:
+            print(date, 'Missing data')
+        else:
+            highs_sitka.append(high)
+            dates_sitka.append(date)
+            lows_sitka.append(low)
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red', alpha=0.5)
-plt.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='black', alpha=0.1)
+plt.plot(dates_death, highs_death, c='red', alpha=0.5)
+plt.plot(dates_death, lows_death, c='red', alpha=0.5)
+plt.fill_between(dates_death, highs_death, lows_death, facecolor='red', alpha=0.1)
+plt.plot(dates_sitka, highs_sitka, c='blue', alpha=0.5)
+plt.plot(dates_sitka, lows_sitka, c='blue', alpha=0.5)
+plt.fill_between(dates_sitka, highs_sitka, lows_sitka, facecolor='blue', alpha=0.1)
 plt.title('Daily high and low temperatures-2021', fontsize=24)
 plt.xlabel('', fontsize=16)
 # 绘制斜制标签，确保太长的标签能完整显示
