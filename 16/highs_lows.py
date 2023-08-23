d4239b8f09029b4045d9e177a4c53a6b8818da1e
大王3111 <1797094806@qq.com>
2023-08-23 19:21:30 +0800

    8.23 19:21:00

--- 16/highs_lows.py
+++ 16/highs_lows.py
@@ -31,29 +31,35 @@ with open(filename_sitka) as f:
     highs_sitka = []
     dates_sitka = []
     lows_sitka = []
+    awnds_sitka = []
     for row in reader:
         try:
+            row[6] = (int(row[7])+int(row[8]))/2
             date = datetime.strptime(row[2], "%Y/%m/%d")
             high = int(row[7])
             low = int(row[8])
+            awnd_sitka = float(row[3])
         except ValueError:
             print(date, 'Missing data')
         else:
             highs_sitka.append(high)
             dates_sitka.append(date)
             lows_sitka.append(low)
+            awnds_sitka.append(awnd_sitka)
 # 根据数据绘制图形
-fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates_death, highs_death, c='red', alpha=0.5)
-plt.plot(dates_death, lows_death, c='red', alpha=0.5)
-plt.fill_between(dates_death, highs_death, lows_death, facecolor='red', alpha=0.1)
-plt.plot(dates_sitka, highs_sitka, c='blue', alpha=0.5)
-plt.plot(dates_sitka, lows_sitka, c='blue', alpha=0.5)
-plt.fill_between(dates_sitka, highs_sitka, lows_sitka, facecolor='blue', alpha=0.1)
-plt.title('Daily high and low temperatures-2021', fontsize=24)
+# fig = plt.figure(dpi=128, figsize=(10, 6))
+# plt.plot(dates_death, highs_death, c='red', alpha=0.5)
+# plt.plot(dates_death, lows_death, c='red', alpha=0.5)
+# plt.fill_between(dates_death, highs_death, lows_death, facecolor='red', alpha=0.1)
+# 绘制的是awnd
+# plt.plot(dates_sitka, highs_sitka, c='blue', alpha=0.5)
+# plt.plot(dates_sitka, lows_sitka, c='blue', alpha=0.5)
+# plt.fill_between(dates_sitka, highs_sitka, lows_sitka, facecolor='blue', alpha=0.1)
+plt.plot(dates_sitka, awnds_sitka, c='blue', alpha=0.5)
+plt.title('Daily awnd -2021', fontsize=24)
 plt.xlabel('', fontsize=16)
 # 绘制斜制标签，确保太长的标签能完整显示
-fig.autofmt_xdate()
+#fig.autofmt_xdate()
 plt.ylabel('Temperature(F)', fontsize=16)
 plt.tick_params(axis='y', which='minor', labelsize=16, reset=True)
 
---
+++ 16/world_population.py
@@ -0,0 +1,16 @@
+# import json
+#
+# # 将数据加载到一个列表中
+# filename = 'population_data.json'
+# with open(filename) as f:
+#     pop_data = json.load(f)
+# # 打印每个国家2010年的人口数量
+# for pop_dict in pop_data:
+#     if pop_dict['Year'] == '2010':
+#         country_name = pop_dict['Country Name']
+#         population = int(float(pop_dict['Value']))
+#         print(country_name + ": " + str(population))
+import pygal_maps_world.maps
+from pygal_maps_world.i18n import COUNTRIES
+for country_code in sorted(COUNTRIES.keys()):
+    print(country_code, COUNTRIES[country_code])
